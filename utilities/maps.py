39023aa002d68f502215a64080c1b352a46b3914
Fiori Bazarello <fioribazarello@gmail.com>
2020-04-12 14:50:12 -0300

    Finalização da pesquisa de rotas

--- utilities/maps.py
+++ utilities/maps.py
@@ -1,13 +1,23 @@
 
 import webbrowser
+import re
 import modules.sounds as sounds
+
+def findRouteLocation(terms):
+  firstPattern = "de (.*?)\ para"
+  first = re.search(firstPattern, terms).group(1)
+  
+  secondPattern = "para (.*?)$"
+  second = re.search(secondPattern, terms).group(1)
+
+  return [first, second]
 
 def searchLocation(terms):
   sounds.playSound('searching_location')
   webbrowser.open_new_tab('https://www.google.com/maps/search/{0}'.format(terms))
 
 def searchRoute(terms):
-  print(terms)
+  places = findRouteLocation(terms)
   sounds.playSound('searching_route')
-  webbrowser.open_new_tab('https://www.google.com/maps/dir/{0}/{1}'.format('Osasco', 'Cuzco'))
+  webbrowser.open_new_tab('https://www.google.com/maps/dir/{0}/{1}'.format(places[0], places[1]))
   
